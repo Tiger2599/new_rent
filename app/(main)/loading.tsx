306b10621d2d24abd5a8b9b14a4050737538cfd3
Tiger2599 <136677647+Tiger2599@users.noreply.github.com>
2026-07-11 18:21:53 +0530

    clean old project

--- app/(main)/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { PageSpinner } from '@/components/Spinner';
-
-export default function MainLoading() {
-  return <PageSpinner />;
-}
--- lib/models/index.ts
+++
@@ -1,8 +0,0 @@
-export { User, type IUser, type Role, type IPermissions } from './User';
-export { Property, type IProperty } from './Property';
-export { Flat, type IFlat } from './Flat';
-export { Tenant, type ITenant } from './Tenant';
-export { RentPayment, type IRentPayment } from './RentPayment';
-export { Expense, type IExpense } from './Expense';
-export { Note, type INote } from './Note';
-export { OtherIncome, type IOtherIncome } from './OtherIncome';
